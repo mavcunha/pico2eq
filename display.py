ef825958536b06ee1822dc13dc18eb0318da5fbe
Marco Valtas <mavcunha@gmail.com>
2022-10-30 00:03:59 -0400

    Color in chart is per column, not global.
    I made a mistake using renewables for the chart color,
    but this value is global and will change the whole chart.
    What I want is that if a column is lower than 50%, it should be
    red. But, because the % to pixel conversion, numbers such as 49.80%
    might render a LED red and a chart column green.

--- display.py
+++ display.py
@@ -75,8 +75,8 @@ class Display:
             self.dot_on = True
             self.dot(color)
         
-    def _column_color(self, renewables):
-        if renewables < 50:
+    def _column_color(self, heigth):
+        if heigth < 32:
             return RED
         else:
             return GREEN
@@ -97,7 +97,7 @@ class Display:
         _DISPLAY.line(0, 72, 250, 72)
         self.chart.add(renewables)
         for (x, y, w, h) in self.chart.render():
-            _DISPLAY.set_pen(self._column_color(renewables))
+            _DISPLAY.set_pen(self._column_color(h))
             _DISPLAY.rectangle(x, y, w, h)
 
     def intensity(self, intensity):
